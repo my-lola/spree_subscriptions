387e42d2279a94a9a52396437970a07e3f87a064
Bryan Mahoney <bryan@goDynamo.com>
2015-06-25 15:29:27 -0400

    WIP - Get specs passing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,9 +10,9 @@ FactoryGirl.find_definitions
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
 
 # Requires factories defined in spree_core
+require 'spree/testing_support/factories'
 require 'spree/testing_support/factories'
 require 'spree/testing_support/controller_requests'
 require 'spree/testing_support/authorization_helpers'
@@ -55,3 +55,5 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
   end
 end
+
+Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
